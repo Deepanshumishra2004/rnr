3b81f9d58e3c18a9d3c9330d9e2d7f4695dd9ed9
Deepanshumishra2004 <deepanshumishra2004@gmail.com>
2025-10-19 03:41:30 +0530

    worflows-CRUD-11

---
+++ features/workflows/server/prefetch.ts
@@ -0,0 +1,11 @@
+import { prefetch, trpc } from "@/trpc/server";
+import { inferInput } from "@trpc/tanstack-react-query";
+
+type Input = inferInput<typeof trpc.workflow.getMany>;
+
+
+// prefetch all workflows
+
+export const prefetchWorkflow=( params : Input )=>{
+    return prefetch(trpc.workflow.getMany.queryOptions(params));
+}
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,33 +1,8 @@
-import { inngest } from '@/inngest/client';
-import { baseProcedure, createTRPCRouter, preminumProcedure, ProtectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { google } from "@ai-sdk/google";
-import { generateText } from 'ai';
+import { workflowsRouter } from '@/features/workflows/server/route';
+import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
-
-  textAi: preminumProcedure.mutation(async()=>{
-    await inngest.send({
-      name : 'execute/ai'
-    })
-
-    return { success : true, message : 'job queued'}
-  }),
-
-  getWorkflow : ProtectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany()
-  }),
-
-  createWorkflow : ProtectedProcedure.mutation(async() => {
-      await inngest.send({
-        name : "test/hello.world",
-        data : {
-          email : 'deimaria@gmail.com',
-        },
-      });
-      return { success:true , messsage : 'job queued' }
-  })
-
+  workflow : workflowsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
